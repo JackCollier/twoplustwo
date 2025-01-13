8811c77c26d571cc238adfb1479a81b99255bf00
JacvaSipt <121406496+JacvaSipt@users.noreply.github.com>
2025-01-13 15:05:09 +0000

    create calculation service to store and retrieve calculations

--- src/app/services/calculation.service.ts
+++ src/app/services/calculation.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Calculation {
+  id?: number;
+  number1: number;
+  number2: number;
+  result: number;
+  timestamp: string;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CalculationService {
+  private endPointUrl = 'http://localhost:3000/calculations'; 
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getCalculations(): Observable<Calculation[]> {
+    return this.http.get<Calculation[]>(this.endPointUrl);
+  }
+
+  addCalculation(calculation: Calculation): Observable<Calculation> {
+    return this.http.post<Calculation>(this.endPointUrl, calculation);
+  }
+
 }
